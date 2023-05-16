1c1070f46fd84ceed35d951fa4c17a65706b902c
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-05-16 19:01:41 +0300

    fixed links image

--- src/data/data.js
+++ src/data/data.js
@@ -1,38 +1,46 @@
+import newBalance574 from '../assets/newBalance574.jpg';
+import newBalance920 from '../assets/newBalance920.jpg';
+import newBalance373 from '../assets/newBalance373.jpg';
+import newBalance670 from '../assets/newBalance670.jpg';
+import newBalanceXRacer from '../assets/newBalanceX-Racer.jpg';
+import newBalance5740 from '../assets/newBalance5740.jpg';
+
+
 export const dataPastCollection = [
   {
     id:1,
     name:'newBalance 373',
-    imgSrc:'../assets/newBalance373.jpg',
+    imgSrc: newBalance373,
   },
   {
     id:2,
     name:'newBalance 574',
-    imgSrc:'../assets/newBalance574.jpg',
+    imgSrc: newBalance574,
   },
   {
     id:3,
     name:'newBalance 373',
-    imgSrc:'../assets/newBalance373.jpg',
+    imgSrc: newBalance373,
   },
   {
     id:4,
     name:'newBalance 574',
-    imgSrc:'../assets/newBalance574.jpg',
+    imgSrc: newBalance574,
   },
   {
     id:5,
     name:'newBalance 373',
-    imgSrc:'../assets/newBalance373.jpg',
+    imgSrc: newBalance373,
   },
   {
     id:6,
     name:'newBalance 574',
-    imgSrc:'../assets/newBalance574.jpg',
+    imgSrc: newBalance574,
   },
   {
     id:7,
     name:'newBalance 373',
-    imgSrc:'../assets/newBalance373.jpg',
+    imgSrc: newBalance373,
   },
 ]
 
@@ -40,82 +48,82 @@ export const dataNewCollection =[
   {
     id:3,
     name:'newBalance 670',
-    imgSrc:'../assets/newBalance670.jpg',
+    imgSrc: newBalance670,
   },
   {
     id:4,
     name:'newBalance 920',
-    imgSrc:'../assets/newBalance920.jpg',
+    imgSrc: newBalance920,
   },
   {
     id:3,
     name:'newBalance 670',
-    imgSrc:'../assets/newBalance670.jpg',
+    imgSrc: newBalance670,
   },
   {
     id:4,
     name:'newBalance 920',
-    imgSrc:'../assets/newBalance920.jpg',
+    imgSrc: newBalance920,
   },
   {
     id:3,
     name:'newBalance 670',
-    imgSrc:'../assets/newBalance670.jpg',
+    imgSrc: newBalance670,
   },
   {
     id:4,
     name:'newBalance 920',
-    imgSrc:'../assets/newBalance920.jpg',
+    imgSrc: newBalance920,
   },
   {
     id:3,
     name:'newBalance 670',
-    imgSrc:'../assets/newBalance670.jpg',
+    imgSrc: newBalance670,
   },
   {
     id:4,
     name:'newBalance 920',
-    imgSrc:'../assets/newBalance920.jpg',
+    imgSrc: newBalance920,
   },
   {
     id:3,
     name:'newBalance 670',
-    imgSrc:'../assets/newBalance670.jpg',
+    imgSrc: newBalance670,
   },
   {
     id:4,
     name:'newBalance 920',
-    imgSrc:'../assets/newBalance920.jpg',
+    imgSrc: newBalance920,
   },
   {
     id:3,
     name:'newBalance 670',
-    imgSrc:'../assets/newBalance670.jpg',
+    imgSrc: newBalance670,
   },
   {
     id:4,
     name:'newBalance 920',
-    imgSrc:'../assets/newBalance920.jpg',
+    imgSrc: newBalance920,
   },
   {
     id:3,
     name:'newBalance 670',
-    imgSrc:'../assets/newBalance670.jpg',
+    imgSrc: newBalance670,
   },
   {
     id:4,
     name:'newBalance 920',
-    imgSrc:'../assets/newBalance920.jpg',
+    imgSrc: newBalance920,
   },
   {
     id:3,
     name:'newBalance 670',
-    imgSrc:'../assets/newBalance670.jpg',
+    imgSrc: newBalance670,
   },
   {
     id:4,
     name:'newBalance 920',
-    imgSrc:'../assets/newBalance920.jpg',
+    imgSrc: newBalance920,
   },
 ]
 
@@ -123,51 +131,51 @@ export const dataFutureCollection =[
   {
     id:5,
     name:'newBalance 5740',
-    imgSrc:'../assets/newBalance5740.jpg',
+    imgSrc: newBalance5740,
   },
   {
     id:6,
     name:'newBalance X-Racer',
-    imgSrc:'../assets/newBalanceX-Racer.jpg',
+    imgSrc:newBalanceXRacer,
   },
   {
     id:5,
     name:'newBalance 5740',
-    imgSrc:'../assets/newBalance5740.jpg',
+    imgSrc: newBalance5740,
   },
   {
     id:6,
     name:'newBalance X-Racer',
-    imgSrc:'../assets/newBalanceX-Racer.jpg',
+    imgSrc:newBalanceXRacer,
   },
   {
     id:5,
     name:'newBalance 5740',
-    imgSrc:'../assets/newBalance5740.jpg',
+    imgSrc: newBalance5740,
   },
   {
     id:6,
     name:'newBalance X-Racer',
-    imgSrc:'../assets/newBalanceX-Racer.jpg',
+    imgSrc:newBalanceXRacer,
   },
   {
     id:5,
     name:'newBalance 5740',
-    imgSrc:'../assets/newBalance5740.jpg',
+    imgSrc: newBalance5740,
   },
   {
     id:6,
     name:'newBalance X-Racer',
-    imgSrc:'../assets/newBalanceX-Racer.jpg',
+    imgSrc:newBalanceXRacer,
   },
   {
     id:5,
     name:'newBalance 5740',
-    imgSrc:'../assets/newBalance5740.jpg',
+    imgSrc: newBalance5740,
   },
   {
     id:6,
     name:'newBalance X-Racer',
-    imgSrc:'../assets/newBalanceX-Racer.jpg',
+    imgSrc:newBalanceXRacer,
   }
 ]
