2aae51613d70fb516c6a8fcb63abae608e45e31b
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-05-17 16:57:52 +0300

    added button-back

--- src/scripts/button-back.js
+++ src/scripts/button-back.js
@@ -8,27 +8,9 @@ backToTopButton.addEventListener('click', () => {
 	});
 });
 
-// Active navigation link
-const navLinks = Array.from(document.querySelectorAll('.category-menu a'));
-
-navLinks.forEach((link) => {
-	link.addEventListener('click', (e) => {
-		e.preventDefault();
-		const id = e.target.getAttribute('href');
-		const section = document.querySelector(id);
-		const topOffset = section.offsetTop - 100; // account for sticky header
-		window.scrollTo({
-			top: topOffset,
-			behavior: 'smooth',
-		});
-		navLinks.forEach((link) => link.classList.remove('active'));
-		e.target.classList.add('active');
-	});
-});
-
 // Show back to top button when scrolling
 window.addEventListener('scroll', () => {
-	if (window.scrollY > 200) {
+	if (window.scrollY > 350) {
 		backToTopButton.style.display = 'block';
 	} else {
 		backToTopButton.style.display = 'none';
