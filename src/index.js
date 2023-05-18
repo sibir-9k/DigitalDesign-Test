94917f77780bda00153a150a23fec7a050560407
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-05-18 23:15:22 +0300

    added new links

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './scripts/toggle-theme.js'
 import './scripts/button-back.js'
 import './scripts/create-cards.js'
 import './scripts/modal.js'
-
+import './scripts/navigate-link.js'
+import './scripts/burger-menu.js'
