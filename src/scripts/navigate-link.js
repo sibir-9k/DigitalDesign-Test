04fdfac41724c41d2f480d7805d5de3b6c6362e3
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-05-18 23:14:01 +0300

    added scroll for fixed navigate

---
+++ src/scripts/navigate-link.js
@@ -0,0 +1,17 @@
+// Active navigation link
+const navLinks = Array.from(document.querySelectorAll('.category-menu a'));
+console.log(navLinks);
+navLinks.forEach((link) => {
+	link.addEventListener('click', (e) => {
+		e.preventDefault();
+		const id = e.target.getAttribute('href');
+		const section = document.querySelector(id);
+		const topOffset = section.offsetTop - 100; // account for sticky header
+		window.scrollTo({
+			top: topOffset,
+			behavior: 'smooth',
+		});
+		navLinks.forEach((link) => link.classList.remove('active'));
+		e.target.classList.add('active');
+	});
+});
