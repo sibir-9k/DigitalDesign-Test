3dc076613fb94849cf565bec0cdcb0ad7e8e7496
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-05-20 14:23:42 +0300

    save theme after reload

--- src/scripts/toggle-theme.js
+++ src/scripts/toggle-theme.js
@@ -1,6 +1,16 @@
 const themeToggle = document.querySelector('.theme-toggle');
 const body = document.querySelector('body');
 
+const savedTheme = sessionStorage.getItem('theme');
+if (savedTheme === 'dark') {
+	body.classList.add('dark-theme');
+}
+
 themeToggle.addEventListener('click', () => {
 	body.classList.toggle('dark-theme');
+	if (body.classList.contains('dark-theme')) {
+		sessionStorage.setItem('theme', 'dark');
+	} else {
+		sessionStorage.removeItem('theme');
+	}
 });
