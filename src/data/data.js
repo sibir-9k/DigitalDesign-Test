7647bbb87fb96bfa042aa3bad2a4fcbdae1b00c8
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-05-20 14:21:02 +0300

    update file

--- src/data/data.js
+++ src/data/data.js
@@ -11,36 +11,43 @@ export const dataPastCollection = [
     id:1,
     name:'newBalance 373',
     imgSrc: newBalance373,
+    data: "06.06.2000"
   },
   {
     id:2,
     name:'newBalance 574',
     imgSrc: newBalance574,
+    data: "01.01.1999"
   },
   {
     id:3,
     name:'newBalance 373',
     imgSrc: newBalance373,
+    data: "02.03.2004"
   },
   {
     id:4,
     name:'newBalance 574',
     imgSrc: newBalance574,
+    data: "03.04.2005"
   },
   {
     id:5,
     name:'newBalance 373',
     imgSrc: newBalance373,
+    data: "05.06.2007"
   },
   {
     id:6,
     name:'newBalance 574',
     imgSrc: newBalance574,
+    data: "08.09.2010"
   },
   {
     id:7,
     name:'newBalance 373',
     imgSrc: newBalance373,
+    data: "11.12.2013"
   },
 ]
 
@@ -49,81 +56,97 @@ export const dataNewCollection =[
     id:3,
     name:'newBalance 670',
     imgSrc: newBalance670,
+    data: "30.12.2020"
   },
   {
     id:4,
     name:'newBalance 920',
     imgSrc: newBalance920,
+    data: "29.11.2021"
   },
   {
     id:3,
     name:'newBalance 670',
     imgSrc: newBalance670,
+    data: "28.10.2022"
   },
   {
     id:4,
     name:'newBalance 920',
     imgSrc: newBalance920,
+    data: "27.09.2023"
   },
   {
     id:3,
     name:'newBalance 670',
     imgSrc: newBalance670,
+    data: "26.08.2022"
   },
   {
     id:4,
     name:'newBalance 920',
     imgSrc: newBalance920,
+    data: "25.07.2022"
   },
   {
     id:3,
     name:'newBalance 670',
     imgSrc: newBalance670,
+    data: "24.06.2022"
   },
   {
     id:4,
     name:'newBalance 920',
     imgSrc: newBalance920,
+    data: "23.05.2022"
   },
   {
     id:3,
     name:'newBalance 670',
     imgSrc: newBalance670,
+    data: "22.04.2022"
   },
   {
     id:4,
     name:'newBalance 920',
     imgSrc: newBalance920,
+    data: "21.03.2022"
   },
   {
     id:3,
     name:'newBalance 670',
     imgSrc: newBalance670,
+    data: "10.02.2000"
   },
   {
     id:4,
     name:'newBalance 920',
     imgSrc: newBalance920,
+    data: "24.05.2004"
   },
   {
     id:3,
     name:'newBalance 670',
     imgSrc: newBalance670,
+    data: "13.08.2010"
   },
   {
     id:4,
     name:'newBalance 920',
     imgSrc: newBalance920,
+    data: "17.09.2009"
   },
   {
     id:3,
     name:'newBalance 670',
     imgSrc: newBalance670,
+    data: "04.06.1998"
   },
   {
     id:4,
     name:'newBalance 920',
     imgSrc: newBalance920,
+    data: "02.02.2000"
   },
 ]
 
@@ -132,50 +155,60 @@ export const dataFutureCollection =[
     id:5,
     name:'newBalance 5740',
     imgSrc: newBalance5740,
+    data: "22.04.2008"
   },
   {
     id:6,
     name:'newBalance X-Racer',
     imgSrc:newBalanceXRacer,
+    data: "24.07.2009"
   },
   {
     id:5,
     name:'newBalance 5740',
     imgSrc: newBalance5740,
+    data: "14.11.1990"
   },
   {
     id:6,
     name:'newBalance X-Racer',
     imgSrc:newBalanceXRacer,
+    data: "25.08.1990"
   },
   {
     id:5,
     name:'newBalance 5740',
     imgSrc: newBalance5740,
+    data: "15.10.1999"
   },
   {
     id:6,
     name:'newBalance X-Racer',
     imgSrc:newBalanceXRacer,
+    data: "18.11.2001"
   },
   {
     id:5,
     name:'newBalance 5740',
     imgSrc: newBalance5740,
+    data: "11.09.2001"
   },
   {
     id:6,
     name:'newBalance X-Racer',
     imgSrc:newBalanceXRacer,
+    data: "03.12.2003"
   },
   {
     id:5,
     name:'newBalance 5740',
     imgSrc: newBalance5740,
+    data: "19.03.2005"
   },
   {
     id:6,
     name:'newBalance X-Racer',
     imgSrc:newBalanceXRacer,
+    data: "27.09.1986"
   }
 ]
